6f61b8f6845d3294a9daf0db4869521b8ab6fb75
Ankit Avinash <ankit.avinash@protonmail.com>
2024-08-26 11:48:12 +0530

    update

--- src.py
+++ src.py
@@ -1,37 +1,66 @@
-from tinygrad import Tensor, dtypes
+import os
 
-board = Tensor.zeros(3, 3, 3, dtype=dtypes.int8).contiguous()
-print(board.numpy())
-
-t = 1
-while True:
-    c = tuple(map(int, input("coords: ").split()))
-    if c == (-1,):
-        break
-    board[*c] = t
-    print(board.numpy())
-    t *= -1
+import numpy as np
 
 
-def check(board: Tensor) -> str | None:
+def check(board: np.ndarray) -> int | None:
+    if 0 not in board:
+        return 0
+
     n = board.ndim
 
     for i in range(n):
-        sum = board.sum(axis=i)
-        if 3 in sum.numpy():
-            print("X has won")
-            return
-        if -3 in sum.numpy():
-            print("O has won")
-            return
+        sum = board.sum(axis=i, keepdims=True)
+        if 3 in sum:
+            return 1
+        if -3 in sum:
+            return -1
 
     if n < 2:
-        return
+        return None
 
     for i in range(n):
         for j in range(i + 1, n):
-            check(Tensor(board.numpy().diagonal(axis1=i, axis2=j)))
-            check(Tensor(board.flip(axis=i).numpy().diagonal(axis1=i, axis2=j)))
+            if ret := check(board.diagonal(axis1=i, axis2=j)):
+                return ret
+            if ret := check(np.flip(board, axis=i).diagonal(axis1=i, axis2=j)):
+                return ret
 
 
-check(board)
+n = int(input("Enter num of dimensions: "))
+board = np.zeros((3,) * n, dtype=np.int8)
+
+os.system("clear")
+print(board)
+
+t = True
+while True:
+    print(f"{'X' if t else 'O'}'s turn.")
+
+    c = tuple(map(int, input("Coords: ").split()))
+    if len(c) != n:
+        print("Wrong num of dims. Try again.")
+        continue
+    for e in c:
+        if not 0 <= e < n:
+            print("Wrong input. Try again.")
+            continue
+    if board[*c] != 0:
+        print("Already there. Try again.")
+        continue
+
+    board[*c] = 1 if t else -1
+
+    os.system("clear")
+    print(board)
+
+    if (end := check(board)) is not None:
+        if end == 1:
+            print("X wins")
+        elif end == -1:
+            print("O wins")
+        else:
+            print("Draw")
+        break
+
+    t = not t
